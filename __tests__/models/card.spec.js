bb115dcfb69a830e5602682a4ebfb4360816ba87
Aaron Krause <aaronjkrause@gmail.com>
2017-04-03 12:06:42 -0700

    card model basics and test

---
+++ __tests__/models/card.spec.js
@@ -0,0 +1,9 @@
+import Card from "../../src/models/card"
+
+test('Card should generate a card instance', () => {
+  const testCard = new Card('testShape', 'testPattern', 'testColor', 5)
+
+  const comparison = {shape: 'testShape', pattern: 'testPattern', color: 'testColor', count: 5}
+
+  expect(testCard).toEqual(comparison)
+})
---
+++ src/models/card.js
@@ -0,0 +1,10 @@
+class Card {
+  constructor(shape, pattern, color, count) {
+    this.shape = shape
+    this.pattern = pattern
+    this.color = color
+    this.count = count
+  }
+}
+
+export default Card
