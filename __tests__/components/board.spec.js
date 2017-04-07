810f687ff15e22bc37dc1514d90e488e3f96da70
Aaron Krause <aaronjkrause@gmail.com>
2017-04-07 14:19:17 -0700

    more board tests, still need to do dispatch

--- __tests__/components/board.spec.js
+++ __tests__/components/board.spec.js
@@ -1,20 +1,35 @@
 import React from 'react'
 import { create } from 'react-test-renderer'
+import { shallow } from 'enzyme'
 import { Provider } from 'react-redux'
 import mockStore from '../lib/mockStore'
 
-import Board from 'src/components/board'
+// Container
+import BoardContainer from 'src/components/board'
+// Presentational Component
+import Board from 'src/components/board/board'
 
 let testState
 beforeEach(() => {
   testState = {
     selectedCards: [],
-    board: [[{shape: 'testShape', pattern: 'testPattern', color: 'testColor', count: 'testCount'}]]
+    board: [[{shape: 'oval', pattern: 'solid', color: 'one', count: 1 }]]
   }
 })
 
-test('should render correctly', () => {
-  const testRender = create(<Provider store={mockStore(testState)}><Board /></Provider>)
+test('container render correctly', () => {
+  const testRender = create(<Provider store={mockStore(testState)}><BoardContainer /></Provider>)
 
   expect(testRender).toMatchSnapshot()
+})
+
+test('should check set if three cards are selected', () => {
+  const validateSet = jest.fn()
+  testState.validateSet = validateSet
+  const testRender = shallow(<Board {...testState} />)
+  testState.selectedCards = [null, null, null]
+
+  testRender.setProps(testState)
+
+  expect(validateSet.mock.calls.length).toBe(1)
 })
