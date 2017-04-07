cf2b07b9f1d450d37ee28537907324205856c37e
Aaron Krause <aaronjkrause@gmail.com>
2017-04-07 12:17:38 -0700

    boardAction tests

---
+++ __tests__/actions/boardActions.spec.js
@@ -0,0 +1,35 @@
+
+import {
+  dealBoard,
+  toggleSelect,
+  validateSet
+} from 'src/actions/boardActions'
+
+import {
+  DEAL_BOARD,
+  TOGGLE_SELECT,
+  VALIDATE_SET
+} from 'src/constants'
+
+
+test('dealBoard', () => {
+  const expected = { type: DEAL_BOARD }
+  const result = dealBoard()
+
+  expect(result).toEqual(expected)
+})
+
+test('toggleSelect', () => {
+  const card = { shape: 'test' }
+  const expected = { type: TOGGLE_SELECT, card  }
+  const result = toggleSelect(card)
+
+  expect(result).toEqual(expected)
+})
+
+test('validateSet', () => {
+  const expected = { type: VALIDATE_SET }
+  const result = validateSet()
+
+  expect(result).toEqual(expected)
+})
