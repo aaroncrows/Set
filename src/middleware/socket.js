ed7ee4e7d5dfbf3b19355d12eaefe15ddb3a7f9c
Aaron Krause <aaronjkrause@gmail.com>
2017-05-17 18:20:21 -0700

    handling new game

--- src/middleware/socket.js
+++ src/middleware/socket.js
@@ -1,22 +1,42 @@
 import io from 'socket.io-client'
 import { syncAndValidate, dealBoard } from '../actions/boardActions'
 import { pauseForSelect } from '../actions/setButtonActions'
+import { newGameCreated } from '../actions/newGameFormActions'
 
-const sock = io('http://localhost:8888/')
+let initialized = false
+let socket
 
 const socketMiddleware = ({ getState, dispatch }) => {
-  sock.on('new', cards => dispatch(dealBoard(cards)))
-  sock.on('select', selectedCards => dispatch(syncAndValidate(selectedCards)))
-  sock.on('pauseForSet', () => dispatch(pauseForSelect()))
+  const initializeSocket = () => {
+    if (socket) return
+    socket = io('http://localhost:8888/')
+    socket.on('new', ({ id, cards }) => {
+      dispatch(dealBoard(cards))
+      dispatch(newGameCreated(id))
+    })
+    socket.on('select', selectedCards => dispatch(syncAndValidate(selectedCards)))
+    socket.on('pauseForSet', () => dispatch(pauseForSelect()))
+    socket.on('roomTest', payload => {console.log(payload)})
+  }
+
+  initializeSocket()
 
   return next => (action) => {
     if (!action) return
+    initializeSocket()
+
     const result = next(action)
 
-    if (action.type === 'TOGGLE_SELECT') return sock.emit('set', getState().selectedCards)
-    if (action.type === 'IS_CHOOSING') sock.emit('choosingSet')
+    //if (action.type === 'START_NEW_GAME') initializeSocket()
+
+    if (socket) {
+      if (action.type === 'START_NEW_GAME') socket.emit('newGame', action.userName)
+      if (action.type === 'TOGGLE_SELECT') return socket.emit('set', getState().selectedCards)
+      if (action.type === 'IS_CHOOSING') socket.emit('choosingSet')
+    }
     return result
   }
 }
 
 export default socketMiddleware
+
